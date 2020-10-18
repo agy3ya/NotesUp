20be59b733fe1618608746aec8d260fb16fb0366
Ankit Singh <xyelblaze@gmail.com>
2020-10-18 18:15:14 +0530

    Floating Action Button

--- app/src/main/java/com/example/notes/HomeActivity.java
+++ app/src/main/java/com/example/notes/HomeActivity.java
@@ -3,12 +3,26 @@ package com.example.notes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class HomeActivity extends AppCompatActivity {
+    private FloatingActionButton fabButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        fabButton= findViewById(R.id.fab_button);
+        fabButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
 }
