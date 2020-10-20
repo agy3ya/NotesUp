b9e3aa0eb9dca3070567b64aa5716429f9f33b59
Ankit Singh <xyelblaze@gmail.com>
2020-10-21 00:34:34 +0530

    Added recycler view adapter

--- app/src/main/java/com/example/notes/HomeActivity.java
+++ app/src/main/java/com/example/notes/HomeActivity.java
@@ -7,23 +7,40 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.example.notes.Model.Data;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import org.w3c.dom.Text;
 
 public class HomeActivity extends AppCompatActivity {
     private FloatingActionButton fabButton;
 
     private RecyclerView recyclerView;
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        mAuth=FirebaseAuth.getInstance();
 
+        FirebaseUser mUser=mAuth.getCurrentUser();
+        String uId=mUser.getUid();
+
+        mDatabase= FirebaseDatabase.getInstance().getReference().child("Notes");
+        mDatabase.keepSynced(true);
 
         //Cardview
         recyclerView =findViewById(R.id.recycler_view);
@@ -38,11 +55,55 @@ public class HomeActivity extends AppCompatActivity {
         fabButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(),NoteActivity.class));
+             Intent intent = new Intent(HomeActivity.this, NoteActivity.class);
+             startActivityForResult(intent,2);
+
 
             }
         });
 
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseRecyclerAdapter<Data,MyViewHolder>adapter = new FirebaseRecyclerAdapter<Data, MyViewHolder>(
+                Data.class,
+                R.layout.item_data,
+                MyViewHolder.class,
+                mDatabase
+
+        ) {
+            @Override
+            protected void populateViewHolder(MyViewHolder myViewHolder, Data model, int i) {
+                myViewHolder.setTitle(model.getTitle());
+                myViewHolder.setNote(model.getNote());
+                myViewHolder.setDate(model.getDate());
+
+
+            }
+        };
+            recyclerView.setAdapter(adapter);
+    }
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+        View myView;
+        public MyViewHolder(View itemView){
+            super(itemView);
+            myView=itemView;
+        }
+        public void setTitle(String title){
+            TextView mTitle = myView.findViewById(R.id.title);
+            mTitle.setText(title);
+        }
+        public void setNote(String note){
+            TextView mNote=myView.findViewById(R.id.note);
+            mNote.setText(note);
+        }
+        public void setDate(String date){
+            TextView mDate= myView.findViewById(R.id.my_date);
+            mDate.setText(date);
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/notes/NoteActivity.java
@@ -0,0 +1,75 @@
+package com.example.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.notes.Model.Data;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+
+public class NoteActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+        mAuth=FirebaseAuth.getInstance();
+        FirebaseUser mUser = mAuth.getCurrentUser();
+        String uId = mUser.getUid();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Notes").child(uId);
+
+        TextInputEditText title = findViewById(R.id.my_heading);
+        EditText note = findViewById(R.id.my_note);
+        FloatingActionButton saveButton = findViewById(R.id.save_button);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mTitle = title.getText().toString().trim();
+                String mNote = note.getText().toString().trim();
+
+                if(TextUtils.isEmpty(mTitle)){
+                    title.setError("Required Field");
+                    return;
+                }
+                if(TextUtils.isEmpty(mNote)){
+                    note.setError("Required Field");
+                    return;
+                }
+                String id = mDatabase.push().getKey();
+              //  String date= DateFormat.getInstance().format(new Date());
+                String date = DateFormat.getInstance().format(new Date());
+                Data data = new Data(mTitle,mNote,date,id);
+                mDatabase.child(id).setValue(data);
+                Toast.makeText(getApplicationContext(),"Data Inserted",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+
+
+            }
+        });
+
+    }
+
+
+
+}
